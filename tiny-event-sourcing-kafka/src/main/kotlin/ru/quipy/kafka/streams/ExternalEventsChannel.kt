5ed28080f567c5434cfbb358a2b61f7b119c7328
pvkvetkin <pvkvetkin@gmail.com>
2023-08-25 22:31:36 +0600

    feat: add external events channel

---
+++ tiny-event-sourcing-kafka/src/main/kotlin/ru/quipy/kafka/streams/ExternalEventsChannel.kt
@@ -0,0 +1,34 @@
+package ru.quipy.kafka.streams
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import ru.quipy.domain.ExternalEventRecord
+
+class ExternalEventsChannel {
+    private val eventsChannel: Channel<ExternalEventRecord> = Channel(
+        capacity = Channel.RENDEZVOUS,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
+
+    private val acknowledgesChannel: Channel<Boolean> = Channel(
+        capacity = Channel.RENDEZVOUS,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
+
+    suspend fun sendEvent(eventRecord: ExternalEventRecord) {
+        eventsChannel.send(eventRecord)
+    }
+
+    suspend fun receiveEvent(): ExternalEventRecord {
+        return eventsChannel.receive()
+    }
+
+    suspend fun sendConfirmation(isConfirmed: Boolean) {
+        acknowledgesChannel.send(isConfirmed)
+    }
+
+    suspend fun receiveConfirmation(): Boolean {
+        return acknowledgesChannel.receive()
+    }
+
+}
