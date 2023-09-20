b447fbee0286032c08ff974f08ed8233eb9ab39d
pvkvetkin <pvkvetkin@gmail.com>
2023-09-20 23:14:02 +0600

    fix: move events mapping in kafka producer subscriber

--- tiny-event-sourcing-kafka/src/main/kotlin/ru/quipy/kafka/streams/KafkaProducerSubscriber.kt
+++ tiny-event-sourcing-kafka/src/main/kotlin/ru/quipy/kafka/streams/KafkaProducerSubscriber.kt
@@ -7,6 +7,7 @@ import ru.quipy.domain.Aggregate
 import ru.quipy.domain.Event
 import ru.quipy.domain.Topic
 import ru.quipy.kafka.core.OngoingGroupManager
+import ru.quipy.kafka.registry.DomainGroupRegistry
 import ru.quipy.mapper.EventMapper
 import ru.quipy.streams.AggregateEventStream
 import ru.quipy.streams.StoppableAndDestructible
@@ -21,39 +22,58 @@ import kotlin.reflect.KClass
 class KafkaProducerSubscriber<A : Aggregate, T : Topic>(
     private val aggregateEventStream: AggregateEventStream<A>,
     private val kafkaProducer: KafkaEventProducer<T>,
+    private val groupRegistry: DomainGroupRegistry,
     private val ongoingGroupManager: OngoingGroupManager,
     private val eventMapper: EventMapper,
     private val nameToEventClassFunc: (String) -> KClass<Event<A>>
 ) : StoppableAndDestructible {
 
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(KafkaProducerSubscriber::class.java)
+    }
+
     @Volatile
     private var active = true
 
-    private val logger: Logger = LoggerFactory.getLogger(KafkaProducerSubscriber::class.java)
-
     private val subscriptionCoroutine: Job = CoroutineScope(
         CoroutineName("handlingCoroutine") + Executors.newSingleThreadExecutor()
-            .asCoroutineDispatcher() // todo sukhoa customize
+            .asCoroutineDispatcher()
     ).launch {
         while (active) {
             aggregateEventStream.handleNextRecord { eventRecord ->
-                try {
+                val result = runCatching {
                     val event = payloadToEvent(eventRecord.payload, eventRecord.eventTitle)
+                    val domainGroup = groupRegistry.getGroupFromDomainEvent(event::class)
 
-                    val externalEventRecords = ongoingGroupManager.convertToExternalEventRecords(event, eventRecord.aggregateId.toString())
+                    val aggregationTable = "aggregation-${domainGroup?.simpleName?.lowercase()}-${eventRecord.aggregateId}"
+                    val externalEventRecords = if (domainGroup == null) {
+                        ongoingGroupManager.convertDomainEvent(event)
+                    } else {
+                        ongoingGroupManager.convertDomainGroup(event, domainGroup, aggregationTable)
+                    }
 
-                    kafkaProducer.sendEvents(eventRecord.aggregateId.toString(), externalEventRecords)
+                    if (externalEventRecords != null) {
+                        if (externalEventRecords.isNotEmpty()) {
+                            kafkaProducer.sendEvents(eventRecord.aggregateId.toString(), externalEventRecords)
+                            logger.info("Sent ${externalEventRecords.size} event(s) to Kafka")
+                            if (externalEventRecords.size > 1) {
+                                ongoingGroupManager.removeAggregationTable(aggregationTable)
+                            }
+                        }
+                    }
 
                     true
-                } catch (e: Exception) {
+                }.onFailure { e ->
                     logger.error(
                         "Unexpected exception while handling event in subscriber. Stream: ${aggregateEventStream.streamName}, event record: $eventRecord",
                         e
                     )
-                    false
                 }
+
+                result.getOrDefault(false)
             }
         }
+
     }
 
     private fun payloadToEvent(payload: String, eventTitle: String): Event<A> = eventMapper.toEvent(
