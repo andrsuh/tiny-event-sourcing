033e70f4c863a669dac84a9b779f10327c6a054e
vekajp <vekajp@gmail.com>
2023-12-29 15:08:11 +0300

    remove imports

--- tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/Application.kt
+++ tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/Application.kt
@@ -1,12 +1,7 @@
 package ru.quipy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import ru.quipy.eventstore.factory.MongoClientFactory
-import ru.quipy.eventstore.factory.MongoClientFactoryImpl
 
 @SpringBootApplication
 class Application
