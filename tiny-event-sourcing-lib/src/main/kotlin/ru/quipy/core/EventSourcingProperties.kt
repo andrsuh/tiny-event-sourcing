9c5cd9c943f8f7558142548722e6e6f36ce75cd2
Andrey Sukhovitsky <Andrey.Sukhovitsky@infobip.com>
2024-02-02 00:02:49 +0200

    - create indexes in mongo - enable snapshots by property

--- tiny-event-sourcing-lib/src/main/kotlin/ru/quipy/core/EventSourcingProperties.kt
+++ tiny-event-sourcing-lib/src/main/kotlin/ru/quipy/core/EventSourcingProperties.kt
@@ -1,12 +1,12 @@
 package ru.quipy.core
 
-import kotlin.random.Random
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 class EventSourcingProperties (
     var snapshotFrequency: Int = 10,
+    var snapshotsEnabled: Boolean = false,
     var snapshotTableName: String = "snapshots", // todo sukhoa should be per aggregate
     var streamReadPeriod: Long = 1_000,
     var streamBatchSize: Int = 200,
@@ -16,5 +16,5 @@ class EventSourcingProperties (
     var maxActiveReaderInactivityPeriod: Duration = 5.minutes,
     var recordReadIndexCommitPeriod: Int = 10,
     val eventReaderHealthCheckPeriod: Duration = 5.seconds,
-    var sagasEnabled: Boolean = true
+    var sagasEnabled: Boolean = true,
 )
