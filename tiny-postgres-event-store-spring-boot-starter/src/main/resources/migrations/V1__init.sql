d692c99dc4f3c6c208d587a5262bfbe969caecd9
vekajp <vekajp@gmail.com>
2024-02-26 20:37:52 +0300

    add constraints

--- tiny-postgres-event-store-spring-boot-starter/src/main/resources/migrations/V1__init.sql
+++ tiny-postgres-event-store-spring-boot-starter/src/main/resources/migrations/V1__init.sql
@@ -1,16 +1,17 @@
 create schema if not exists event_sourcing_store;
+
 create sequence event_sourcing_store.event_record_created_at_sequence;
 create table if not exists event_sourcing_store.event_record (
-       id text,
-       aggregate_table_name text,
-       aggregate_id text,
-       aggregate_version bigint,
-       event_title text,
-       payload text,
-       saga_context text,
-       created_at bigint default nextval('event_sourcing_store.event_record_created_at_sequence')
+     id text primary key,
+     aggregate_table_name text,
+     aggregate_id text,
+     aggregate_version bigint,
+     event_title text,
+     payload text,
+     saga_context text,
+     created_at bigint default nextval('event_sourcing_store.event_record_created_at_sequence'),
+     unique (aggregate_id, aggregate_version)
 );
-
 create table if not exists event_sourcing_store.snapshot (
     id text primary key,
     snapshot_table_name text,
@@ -26,7 +27,7 @@ create table if not exists event_sourcing_store.event_stream_read_index (
 );
 
 create table if not exists event_sourcing_store.event_stream_active_readers (
-    id text unique primary key,
+    id text primary key,
     version bigint,
     reader_id text,
     read_position bigint,
