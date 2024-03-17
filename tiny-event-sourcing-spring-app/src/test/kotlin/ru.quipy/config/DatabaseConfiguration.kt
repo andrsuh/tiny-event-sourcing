c9b68781cf785d7ebad91f39ca8c52f572647607
vekajp <vekajp@gmail.com>
2024-03-17 18:46:17 +0300

    bugfix + refactor = almost final version<3

--- tiny-event-sourcing-spring-app/src/test/kotlin/ru.quipy/config/DatabaseConfiguration.kt
+++ tiny-event-sourcing-spring-app/src/test/kotlin/ru.quipy/config/DatabaseConfiguration.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
-import javax.sql.DataSource
 
 @Configuration
 class DatabaseConfiguration {
@@ -17,7 +16,7 @@ class DatabaseConfiguration {
         @Value("\${jdbc.username:tiny_es}") username: String,
         @Value("\${jdbc.password:tiny_es}") password: String,
         @Value("\${schema:event_sourcing_store}") schema: String)
-        : DataSource {
+        : HikariDataSource {
         val container = PostgreSQLContainer(DockerImageName.parse("postgres:14.9-alpine")).apply {
             withDatabaseName(dbName)
             withUsername(username)
