308bfc0ae0e17a764c116b006e4a52a2ebb5e611
Andrey Sukhovitsky <Andrey.Sukhovitsky@infobip.com>
2024-05-10 12:56:51 +0300

    minor

--- tiny-postgres-event-store-spring-boot-starter/src/main/resources/liquibase/changelog.sql
+++ tiny-postgres-event-store-spring-boot-starter/src/main/resources/liquibase/changelog.sql
@@ -17,7 +17,7 @@ create table if not exists ${schema}.event_record
 );
 
 CREATE INDEX idx_created_at ON event_sourcing_store.event_record(aggregate_table_name, created_at);
-CREATE INDEX idx_aggregate_id_version ON event_sourcing_store.event_record(aggregate_table_name, aggregate_id, aggregate_version);
+CREATE INDEX idx_aggregate_id_version ON event_sourcing_store.event_record(aggregate_id, aggregate_version);
 
 create table if not exists ${schema}.snapshot
 (
