5a6f609578489d484bacadc352bb68fa845807cc
vekajp <vekajp@gmail.com>
2024-05-12 22:19:53 +0300

    version 2.6.4

--- tiny-postgresql-event-store/src/main/resources/liquibase/changelog.sql
+++ tiny-postgresql-event-store/src/main/resources/liquibase/changelog.sql
@@ -14,6 +14,10 @@ create table if not exists ${schema}.event_record
     created_at           bigint default nextval('${schema}.event_record_created_at_sequence'),
     unique (aggregate_id, aggregate_version)
 );
+
+CREATE INDEX idx_created_at ON event_sourcing_store.event_record(aggregate_table_name, created_at);
+CREATE INDEX idx_aggregate_id_version ON event_sourcing_store.event_record(aggregate_id, aggregate_version);
+
 create table if not exists ${schema}.snapshot
 (
     id                         text primary key,
